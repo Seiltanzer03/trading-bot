b670c304a296a0881f65ddabe9fb679c353ea563
Seiltanzer03 <tizinallergia1@gmail.com>
2026-02-20 02:57:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -174,4 +174,4 @@ async def shutdown():
     await application.shutdown()
 
 if __name__ == "__main__":
-    uvicorn.run("bot:app_fastapi", host="0.0.0.0", port=PORT)
+    uvicorn.run("bot:app", host="0.0.0.0", port=PORT)
